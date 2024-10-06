2837a19582f0226cf7d6e67e64648fbbfa652dd6
Berk <mberktuncer@gmail.com>
2024-10-06 14:33:06 +0300

    SQL Exercises 6 added.

---
+++ SQL/SQL Exercises 6.sql
@@ -0,0 +1,10 @@
+select AVG(rental_rate) from film;
+
+select count(*) from film
+where title like 'C%';
+
+select MAX(length) from film 
+where rental_rate = 0.99;
+
+select count(distinct replacement_cost) from film
+where length > 150;
