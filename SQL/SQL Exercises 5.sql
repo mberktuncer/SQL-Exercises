25c42e6186a1a5f260301c02a703f0777e2242b0
Berk <mberktuncer@gmail.com>
2024-10-06 14:17:09 +0300

    SQL Exercises 5 added.

---
+++ SQL/SQL Exercises 5.sql
@@ -0,0 +1,15 @@
+select * from film
+where title like '%n'
+order by length desc
+limit 5;
+
+select * from film
+where title like '%n'
+order by length 
+offset 5
+limit 5;
+
+select * from customer
+where store_id = 1
+order by last_name 
+limit 4;
