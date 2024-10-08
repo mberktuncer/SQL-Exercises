bef43686b62d0bd35a71f4e68335ad1797ac970f
Berk <mberktuncer@gmail.com>
2024-10-08 13:36:12 +0300

    SQL Exercises 9 added.

--- SQL/SQL Exercises 1.sql
+++ SQL/SQL Exercises 1.sql
@@ -9,6 +9,18 @@ create table author(
 select * from author;
 
 
-insert into author(
+insert into author(first_name, last_name, email, birthday)
+values
+	('Haruki', 'Murakami', 'haruki@murakami.com', '1948-11-07'),
+	('Sabahattin', 'Ali', 'sabahattin@ali.com', '1914-07-11'),
+	('Orhan', 'Pamuk', 'orhan@pamuk.com', '1950-04-14'),
+	('Halide Edip', 'Adıvar', 'halide@edip.com', '1884-07-11'),
+	('Zygmunt', 'Bauman', 'zygmunt@ali.com', '1911-07-12');
+
+
+update author
+set last_name = 'Updated'
+where first_name = 'Odetta'
+returning *;
+
 	
-)
